d6b0506cf2204b9d47ba6de101e8a28548e62cda
Luisedu012 <luiseduardorm012@gmail.com>
2025-04-12 23:23:23 -0300

    feat: adicionar salvamento do arquivo json dos demais custos do Tesouro nacional.

--- src/ETL/Extract/request_demais_custos.py
+++ src/ETL/Extract/request_demais_custos.py
@@ -5,6 +5,7 @@ Código para Realizar requisição na consulta de demais custos do tesouro nacio
 """
 import os
 import requests
+import json
 
 from typing import Optional
 from dotenv import load_dotenv
@@ -38,6 +39,9 @@ class DemaisCustos:
         self.org_level_3 = org_level_3
         self.url = os.getenv("DEM_URL_DEMAIS")
 
+        self.raw_data_dir = os.path.join(os.getcwd(), "src", "raw_data")
+        os.makedirs(self.raw_data_dir, exist_ok=True)
+
     def _build_params(self, year: int, month: int) -> dict:
         """
         Construindo as querys para Requisições da API
@@ -57,6 +61,19 @@ class DemaisCustos:
             "organizacao_n2": self.org_level_2,
             "organizacao_n3": self.org_level_3,
         }
+    
+    def _save_raw_json(self, data: list) -> None:
+        """
+        Salva todos os dados acumulados em um único arquivo JSON.
+
+        Parameters:
+            data (list): Lista contendo todos os dados a serem salvos.
+        """
+        filename = "demais_custos_totais.json"
+        filepath = os.path.join(self.raw_data_dir, filename)
+
+        with open(filepath, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
 
     def fetch_data(self) -> None:
         """
@@ -66,6 +83,7 @@ class DemaisCustos:
 
         """
         total_data_count = 0
+        all_data = []
 
         for year in range(self.start_yaer, self.end_year + 1):
             for month in range(1, 13):
@@ -75,13 +93,18 @@ class DemaisCustos:
                     response.raise_for_status()
 
                     data = response.json()
-                    data_count = len(data.get("items", []))
+                    all_data.extend(data.get("items", []))
 
-                    print(f"Year: {year}, Month: {month} - Data_count: {data_count}")
-                    total_data_count += data_count
+
+                    #data_count = len(data.get("items", []))
+
+                    print(f"Year: {year}, Month: {month} - Data_count: {len(data.get('items', []))}")
+                    #total_data_count += data_count
+                    total_data_count += len(data.get("items", []))
                 except requests.exceptions.RequestException as e:
                     print(f"An error occurred for {year}-{month}: {e}")
 
+        self._save_raw_json(all_data)
         print(f"\nTotal Data Colleted: {total_data_count}")
 
 
